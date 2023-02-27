79aa181c77d33679ef1ef37d0c2fee794c585dfe
Yurishizu9 <improvedgamer1@gmail.com>
2023-02-27 13:35:31 +0000

    created manual_rps.py

---
+++ manual_rps.py
@@ -0,0 +1,9 @@
+import random
+
+def get_computer_choice():
+    return random.choice(["Rock", "Paper", "Scissors"])
+
+def get_user_choice():
+    user_input = input('Choose between "Rock", "Paper", "Scissors"')
+
+    
